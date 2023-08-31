f61f55fcd1f7983d1953ba8348056f3298b70a35
Sessh <gusalvescontato@gmail.com>
2023-08-31 15:31:48 -0300

    feat: test completed

--- SchoolNotesEditor/notes_editor/models.py
+++ SchoolNotesEditor/notes_editor/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Student(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     note = models.DecimalField(max_digits=5, decimal_places=2)
 
--- SchoolNotesEditor/notes_editor/views.py
+++ SchoolNotesEditor/notes_editor/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
 
-# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        student_id = request.POST.get('id')  # Get student ID from hidden input
+        student_name = request.POST.get('name')
+        student_note = request.POST.get('note')
+
+        if student_id:  # Check if student ID exists for update
+            student = get_object_or_404(Student, id=student_id)
+            student.name = student_name
+            student.note = student_note
+            student.save()
+        else:
+            new_student = Student(name=student_name, note=student_note)
+            new_student.save()
+        return redirect('home')
+
+    students = Student.objects.all()
+    return render(request, 'students/home.html', {'students': students})
