901cd6289780cc5b22074d7c29adb26fa65763c6
luteroelavoco <luteroelavo90@gmail.com>
2023-08-31 09:41:16 -0300

    fix: typescript

--- src/utils/cleanData.ts
+++ src/utils/cleanData.ts
@@ -1,6 +1,8 @@
-export const cleanData = data => {
+export const cleanData = (
+  data: Record<string, any>[]
+): Record<string, any>[] => {
   return data.map(item => {
-    const newItem = {}
+    const newItem: Record<string, any> = {}
     for (const key in item) {
       if (Object.hasOwnProperty.call(item, key)) {
         const cleanedKey = key.replace(/^[\s\uFEFF\xA0]+/, '') // Remove non-printable characters
